19094143d731da5d0ae6ffa91307380eddc58619
RoseErrik-FS <errose@student.fullsail.edu>
2023-02-12 16:15:24 -0600

    Jest testing with a mock and real test

--- services/apiService.test.js
+++ services/apiService.test.js
@@ -1,7 +1,19 @@
-const { apiService, apiParticipants } = require('./apiService');
+const { apiService, apiTitle } = require("./apiService");
 
-describe("Testing the api calling function", () => {
-    test("should test the apiService Functions", () => {
-        
-    })
-})
+//jest.mock("./apiService");
+
+describe("Testing the API Service", () => {
+  test("should test the apiService Functions", async () => {
+    const result = await apiService();
+    expect(result.data.entries[2].API).toEqual("Cat Facts");
+    expect(result.data.entries[2].Description).toEqual("Daily cat facts");
+    expect(result.data.entries[2].Category).toEqual("Animals");
+  });
+
+  test("should test the apiTitle function", async () => {
+    const result = await apiTitle("AdoptAPet");
+    expect(result.data.entries[0].API).toEqual("AdoptAPet");
+    expect(result.data.entries[0].Description).toEqual("Resource to help get pets adopted")
+    expect(result.data.entries[0].Category).toEqual("Animals");
+  });
+});
